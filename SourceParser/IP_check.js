5b21cbcc7e5e2bac114be6c9a81c5b1d5688947c
oooo <105234248+QXPublic@users.noreply.github.com>
2023-12-06 04:12:59 +0800

    Update IP_check.js

--- SourceParser/IP_check.js
+++ SourceParser/IP_check.js
@@ -1,22 +1,33 @@
 const url = "http://ip234.in/ip.json";
 
 $http.get(url).then(response => {
-    const data = response.body;
-    const ipInfo = JSON.parse(data);
-    
-    if (ipInfo && ipInfo.hasOwnProperty('type')) {
-        // 检测到 IP 类型存在
-        let ipType = ipInfo.type;
-        // 输出 IP 类型
-        console.log(`当前 IP 类型为: ${ipType}`);
-        $done({title: "IP 类型检测", message: `当前 IP 类型为: ${ipType}`});
+    let ipData = response.data;
+    let ipType = ipData.type;
+    let result;
+
+    // Check if we have an IP type returned
+    if (ipType) {
+        result = {
+            title: "IP Type",
+            content: ipType,
+            icon: "globe"
+        };
     } else {
-        // 未检测到 IP 类型
-        console.log("无法检测到当前 IP 类型。");
-        $done({title: "IP 类型检测", message: "无法检测到当前 IP 类型。"});
+        result = {
+            title: "IP Type",
+            content: "Unknown",
+            icon: "exclamationmark.triangle"
+        };
     }
+
+    // Call $done to return the result to Surge
+    $done(result);
 }).catch(error => {
-    // 出现错误
-    console.error(`请求出错: ${error}`);
-    $done({title: "IP 类型检测", message: "请求出错，请检查网络连接。"});
+    // Handle the error
+    let result = {
+        title: "IP Type",
+        content: "Error",
+        icon: "xmark.circle"
+    };
+    $done(result);
 });
