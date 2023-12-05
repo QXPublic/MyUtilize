5d09b45a2a6a1ec256996f4a81622e78ad4545eb
oooo <105234248+QXPublic@users.noreply.github.com>
2023-12-06 04:30:35 +0800

    Update IP_check.js

--- SourceParser/IP_check.js
+++ SourceParser/IP_check.js
@@ -1,33 +1,10 @@
-const url = "http://ip234.in/ip.json";
+const url = 'http://ip234.in/ip.json';
 
-$http.get(url).then(response => {
-    let ipData = response.data;
-    let ipType = ipData.type;
-    let result;
-
-    // Check if we have an IP type returned
-    if (ipType) {
-        result = {
-            title: "IP Type",
-            content: ipType,
-            icon: "globe"
-        };
+$httpClient.get(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        $done(body);
     } else {
-        result = {
-            title: "IP Type",
-            content: "Unknown",
-            icon: "exclamationmark.triangle"
-        };
+        console.error('Failed to fetch content:', error);
+        $done();
     }
-
-    // Call $done to return the result to Surge
-    $done(result);
-}).catch(error => {
-    // Handle the error
-    let result = {
-        title: "IP Type",
-        content: "Error",
-        icon: "xmark.circle"
-    };
-    $done(result);
 });
