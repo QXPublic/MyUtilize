d1f2966f976d74a3a4ce59a97526eca902d13e8f
oooo <105234248+QXPublic@users.noreply.github.com>
2023-12-06 20:21:03 +0800

    Update and rename SourceParser/IP_check.js to SourceParser/Group-Panel/Group-Panel.js

---
+++ SourceParser/Group-Panel/Group-Panel.js
@@ -0,0 +1,12 @@
+#!name=Group-Panel
+#!desc=快速切换节点
+#!system=ios
+# 对应参数：icon：图标，color：图标颜色，group：策略组名称
+# 更新日期：2022.08.24
+# 版本：2.0
+
+[Panel]
+Group-Panel = script-name=Group-Panel,update-interval=1
+
+[Script]
+Group-Panel = type=generic,timeout=10,script-path=777,argument=icon=network&color=#86abee&group=TW🇹🇼
