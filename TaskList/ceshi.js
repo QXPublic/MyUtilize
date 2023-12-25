2321456078542a19883b99d9e94a1050359d8230
oooo <105234248+QXPublic@users.noreply.github.com>
2023-12-25 09:03:15 +0800

    Update ceshi.js

--- TaskList/ceshi.js
+++ TaskList/ceshi.js
@@ -3,22 +3,23 @@ var Echo = JSON.parse($response.body);
 Echo = {
   "success" : true,
   "data" : {
-    "totalCreditGpt4" : 999,
+    "totalCreditGpt4" : 7,
     "isPremium" : true,
-    "totalCreditImage" : 999,
-    "creditGpt4" : 999,
-    "creditImage" : 999,
+    "totalCreditImage" : 10,
+    "creditGpt4" : 7,
+    "creditImage" : 10,
     "totalCredit" : 150,
     "userId" : "7F4C8B872C7649D88A226C02D88DE4A2",
-    "credit" : 999,
-    "creditBard" : 999,
-    "creditGpt4Vision" : 999,
-    "totalCreditGpt4Vision" : 99,
-    "totalCreditBard" : 999,
-    "creditDocument" : 999,
-    "totalCreditDocument" : 999
+    "credit" : 150,
+    "creditBard" : 150,
+    "creditGpt4Vision" : 15,
+    "totalCreditGpt4Vision" : 15,
+    "totalCreditBard" : 150,
+    "creditDocument" : 10,
+    "totalCreditDocument" : 10
   },
   "time" : "2023-12-24T23:33:01.404Z"
 }
+
 
 $done({body: JSON.stringify(Echo)});
