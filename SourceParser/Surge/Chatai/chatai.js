b74785d56a33cf96d382b9221864f39bd5e6f0f0
oooo <105234248+QXPublic@users.noreply.github.com>
2023-12-28 20:41:49 +0800

    Update chatai.js

--- SourceParser/Surge/Chatai/chatai.js
+++ SourceParser/Surge/Chatai/chatai.js
@@ -2,6 +2,7 @@ const chxm1023 = {};
 const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
 const name = "Advanced";
+const name1 = "Unlimited";
 const appid = "com.palligroup.gpt3.yearlyyy";
 
 if (typeof $response == "undefined") {
@@ -16,6 +17,14 @@ if (typeof $response == "undefined") {
 	"expires_date": "2099-09-09T09:09:09Z",
 	"purchase_date": "2022-09-09T09:09:09Z"
 	};
+  const data1 = {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2023-12-24T10:45:38Z",
+        "product_identifier" : "gpt_year",
+        "expires_date" : "2099-12-27T10:45:38Z"
+      };
+      
+  chxm1024.subscriber.entitlements[(name1)] = (data1);
   chxm1024.subscriber.entitlements[(name)] = (data);
   chxm1024.subscriber.subscriptions[(appid)] = {  ...data,	"Author": "chxm1023",	"Telegram": "https://t.me/chxm1023",	"warning": "仅供学习，禁止转载或售卖",	"original_purchase_date": "2022-09-09T09:09:09Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
   chxm1023.body = JSON.stringify(chxm1024);
