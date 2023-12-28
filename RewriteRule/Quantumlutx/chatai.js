25343ed2cf8c12e3781f76fbce159ae7a62507b7
oooo <105234248+QXPublic@users.noreply.github.com>
2023-12-28 22:34:31 +0800

    Update chatai.js

--- RewriteRule/Quantumlutx/chatai.js
+++ RewriteRule/Quantumlutx/chatai.js
@@ -25,7 +25,6 @@ const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body ||
 
 const name = "Advanced";
 const name1 = "Unlimited";
-const name2 = "Premium";
 const appid = "com.palligroup.gpt3.yearlyyy";
 
 if (typeof $response == "undefined") {
@@ -46,14 +45,9 @@ if (typeof $response == "undefined") {
         "product_identifier" : "gpt_year",
         "expires_date" : "2099-12-27T10:45:38Z"
       };
-    const data2 ={
-        "grace_period_expires_date" : null,
-        "purchase_date" : "2023-12-21T17:07:11Z",
-        "product_identifier" : "annual",
-        "expires_date" : "2099-12-24T17:07:11Z"
-      };
+   
   
-  chxm1024.subscriber.entitlements[(name2)] = (data2);
+ 
   chxm1024.subscriber.entitlements[(name1)] = (data1);
   chxm1024.subscriber.entitlements[(name)] = (data);
   chxm1024.subscriber.subscriptions[(appid)] = {  ...data,	"Author": "chxm1023",	"Telegram": "https://t.me/chxm1023",	"warning": "仅供学习，禁止转载或售卖",	"original_purchase_date": "2022-09-09T09:09:09Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
