624313f7fe9479614b1cd9111abea558dd0073bd
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-01 20:04:29 +0800

    Update IP-Check.js

--- SourceParser/Surge/IP-Check/IP-Check.js
+++ SourceParser/Surge/IP-Check/IP-Check.js
@@ -1,36 +1,30 @@
-/*
- * 由@congcong0806编写
- * 原脚本地址：https://github.com/congcong0806/surge-list/blob/master/Script/ipcheck.js
- * 由@Rabbit-Spec修改
- * 更新日期：2022.08.14
- * 版本：1.5
- */
 
-let url = "https://api.ipdata.co/?api-key=64e914705286ef6ac7ee40f52ad2ab69d90401cbbacfdace0948cbca"
+const apiURL = "https://api.ipdata.co/?api-key=64e914705286ef6ac7ee40f52ad2ab69d90401cbbacfdace0948cbca";
 
-$httpClient.get(url, function(error, response, data){
-    let jsonData = JSON.parse(data)
-   /* #let country = jsonData.country
-    #let emoji = getFlagEmoji(jsonData.countryCode)
-    #let city = jsonData.city
-    #let isp = jsonData.isp*/
-    let ip = jsonData.ip
-  body = {
+function onResponse(request, response) {
+  // 发送请求获取数据
+  $httpClient.get(apiURL, (error, response, data) => {
+    if (error || response.status !== 200) {
+      console.error(`Error: ${error}`);
+      $done({});
+    } else {
+      // 确保返回 JSON 数据
+      let jsonData;
+      try {
+        jsonData = JSON.parse(data);
+      } catch (parseError) {
+        console.error("JSON parse error: " + parseError);
+        $done({});
+      }
+      // 向 Surge 响应体中写入数据
+      let IP = jsonData.ip
+      body = {
     title: "节点信息",
-    content: `IP信息：${ip}\n运营商：${ip}\n所在地：${ip}${ip} - ${ip}`,
-    icon: "globe.asia.australia.fill"
-  }
-  $done(body);
-});
-
-function getFlagEmoji(countryCode) {
-      if (countryCode.toUpperCase() == 'TW') {
-    countryCode = 'CN'
-  }
-  const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt())
-  return String.fromCodePoint(...codePoints)
-
+    content: `IP信息：${IP}`,
+      }
+      $done({body});
+    }
+  });
 }
+
+module.exports = {onResponse};
