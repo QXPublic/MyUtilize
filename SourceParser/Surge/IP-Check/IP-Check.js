663b82d4e47c1d535f311bdd11a0a999ef024eb6
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-01 19:53:53 +0800

    Update IP-Check.js

--- SourceParser/Surge/IP-Check/IP-Check.js
+++ SourceParser/Surge/IP-Check/IP-Check.js
@@ -10,14 +10,14 @@ let url = "https://api.ipdata.co/?api-key=64e914705286ef6ac7ee40f52ad2ab69d90401
 
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
-    let country = jsonData.country
-    let emoji = getFlagEmoji(jsonData.countryCode)
-    let city = jsonData.city
-    let isp = jsonData.isp
-    let ip = jsonData.query
+    #let country = jsonData.country
+    #let emoji = getFlagEmoji(jsonData.countryCode)
+    #let city = jsonData.city
+    #let isp = jsonData.isp
+    let ip = jsonData.ip
   body = {
     title: "节点信息",
-    content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}${country} - ${city}`,
+    content: `IP信息：${ip}\n运营商：${ip}\n所在地：${ip}${ip} - ${ip}`,
     icon: "globe.asia.australia.fill"
   }
   $done(body);
