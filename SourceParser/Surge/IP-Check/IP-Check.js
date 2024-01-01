a9450a16517653ac147e2c2d33f90bab5b902a32
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-02 01:40:48 +0800

    Update IP-Check.js

--- SourceParser/Surge/IP-Check/IP-Check.js
+++ SourceParser/Surge/IP-Check/IP-Check.js
@@ -1,30 +1,28 @@
+let url = "http://ip-api.com/json"
 
-const apiURL = "https://api.ipdata.co/?api-key=64e914705286ef6ac7ee40f52ad2ab69d90401cbbacfdace0948cbca";
-
-function onResponse(request, response) {
-  // 发送请求获取数据
-  $httpClient.get(apiURL, (error, response, data) => {
-    if (error || response.status !== 200) {
-      console.error(`Error: ${error}`);
-      $done({});
-    } else {
-      // 确保返回 JSON 数据
-      let jsonData;
-      try {
-        jsonData = JSON.parse(data);
-      } catch (parseError) {
-        console.error("JSON parse error: " + parseError);
-        $done({});
-      }
-      // 向 Surge 响应体中写入数据
-      let IP = jsonData.ip
-      body = {
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
     title: "节点信息",
-    content: `IP信息：${IP}`,
-      }
-      $done({body});
-    }
-  });
-}
+    content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}${country} - ${city}`,
+    icon: "globe.asia.australia.fill"
+  }
+  $done(body);
+});
 
-module.exports = {onResponse};
+function getFlagEmoji(countryCode) {
+      if (countryCode.toUpperCase() == 'TW') {
+    countryCode = 'CN'
+  }
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char => 127397 + char.charCodeAt())
+  return String.fromCodePoint(...codePoints)
+
+}
