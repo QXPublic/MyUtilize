5d52cadc201be20da6fe578f466c7cda57ec4706
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-04 22:30:40 +0800

    Update WorkingCopy.js

---
+++ SourceParser/Quantumlutx/WorkingCopy/WorkingCopy.js
@@ -0,0 +1,17 @@
+/*
+Working Copy使用方法：
+
+利用 GitHub Education 解锁 Working Copy
+
+启用此脚本后再 Safari 打开 https://workingcopy.app/education/ 点击「tap here to access all pro features.」中的 here 跳转到 Working Copy，登录自己的 GitHub 账号即可解锁
+
+[reweite_local]
+^https?:\/\/education\.github\.com\/api\/user$ url script-response-body 
+
+
+[MitM]
+hostname = education.github.com
+
+ */
+
+$done({body: '{"student": true}'})
