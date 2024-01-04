e8d2c282076dd23290a2d3ddd0757353d16c7293
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-04 22:25:27 +0800

    Update WorkingCopy.js

--- SourceParser/Surge/WorkingCopy/WorkingCopy.js
+++ SourceParser/Surge/WorkingCopy/WorkingCopy.js
@@ -6,7 +6,7 @@ Working Copy使用方法：
 启用此脚本后再 Safari 打开 https://workingcopy.app/education/ 点击「tap here to access all pro features.」中的 here 跳转到 Working Copy，登录自己的 GitHub 账号即可解锁
 
 [reweite_local]
-^https?:\/\/education\.github\.com\/api\/user$ url script-response-body https://raw.githubusercontent.com/QXPublic/MyUtilize/main/SourceParser/Surge/WorkingCopy/WorkingCopy.js
+^https?:\/\/education\.github\.com\/api\/user url script-response-body https://raw.githubusercontent.com/QXPublic/MyUtilize/main/SourceParser/Surge/WorkingCopy/WorkingCopy.js
 
 
 [MitM]
