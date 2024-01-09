c2e2c46219cf739c5c4862536dbb4ffa77d804db
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-09 23:43:04 +0800

    Update Glamme.js

--- RewriteRule/Quantumlutx/Glamme.js
+++ RewriteRule/Quantumlutx/Glamme.js
@@ -16,12 +16,26 @@ if (url.indexOf('user/initialise') != -1) {
     body = JSON.stringify(obj);
 } else if (url.indexOf('vip/vip_card') != -1) {
     var obj = JSON.parse(body);
-    obj.response.vip_info.is_vip = true;
-    obj.response.vip_info.is_auto_renew = true;
-    obj.response.vip_info.start_at = 1704806188.7748761;
-    obj.response.vip_info.expired_at = 4100731932.7748761;
-    obj.response.vip_info.vip_level = 1;
-    body = JSON.stringify(obj);
+    obj = {
+  "response" : {
+    "vip_info" : {
+      "is_vip" : true,
+      "is_auto_renew" : true,
+      "start_at" : 1704806188.7748761,
+      "is_trial" : false,
+      "is_vip_in_history" : false,
+      "sku" : "",
+      "expired_at" : 4100731932.7748761,
+      "vip_level" : 1,
+      "user_id" : 10173151
+    }
+  },
+  "result" : 1,
+  "uri" : "\/api\/v3\/vip\/vip_card",
+  "component" : "vip",
+  "message" : "success",
+  "timestamp" : 1704807067
+};
 
 }
 $done({ body });
