36666e5edb8885c48aad18768f45447e30877d5c
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-10 21:32:34 +0800

    Update and rename TikTok_Rewrite.conf to TikTok_Rewrite.js

--- Rewrite/Quantumlutx/TikTok_Rewrite.js
+++ Rewrite/Quantumlutx/TikTok_Rewrite.js
@@ -1,9 +1,12 @@
 # TikTok TaiWan
+/*
+###换区：在[rewrite_local]中添加下句重写，并将CN改为想看的国家/地区的2位大写英文简写 JP（日本）｜KR（韩国）｜UK（英国）｜US（美国）｜TW（台湾）
+/*
 
+[mitm]
 hostname = *.tiktokv.com, *.byteoversea.com, *.tik-tokapi.com,-*snssdk.com, -*amemv.com
 
-###换区：在[rewrite_local]中添加下句重写，并将CN改为想看的国家/地区的2位大写英文简写 JP（日本）｜KR（韩国）｜UK（英国）｜US（美国）｜TW（台湾）
-
+[rewrite_local]
 (?<=_region=)CN(?=&) url 307 TW
 (?<=&mcc_mnc=)4 url 307 2
 ^(https?:\/\/(tnc|dm)[\w-]+\.\w+\.com\/.+)(\?)(.+) url 302  $1$3
