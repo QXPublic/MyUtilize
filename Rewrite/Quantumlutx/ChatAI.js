9f664a1812ac49046700ce75a026b8ae0a63f422
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-12 15:09:52 +0800

    Update ChatAI.js

--- Rewrite/Quantumlutx/ChatAI.js
+++ Rewrite/Quantumlutx/ChatAI.js
@@ -11,21 +11,20 @@ var body = $response.body;
 var url = $request.url;
 //会员
 if (url.indexOf('ai/get-active-subscription-data') != -1) {
+    if ($response.status === 304) {
+    $response.status = 200;
     var obj = JSON.parse(body);
     obj.data.free_type = 3;
     obj.data.ai_subscription_end_time = "4100731932000";
     obj.data.ai_subscription_active = 1;
     body = JSON.stringify(obj);
 } else if (url.indexOf('users/login-register-user') != -1) {
-if ($response.status === 304) {
-  $response.status = 200;
+
   var obj = JSON.parse(body);
     obj.data.ai_subscription_active = 1;
     obj.data.ai_subscription_end_time = "4100731932000";
     obj.data.free_type = 3 ;
     body = JSON.stringify(obj);
-} 
-
 }
 $done({ body });
 
