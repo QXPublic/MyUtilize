9db18bf49ad3e0b853e2d4a6b5ffd3e94a8ebcca
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-12 21:41:36 +0800

    Update Gchat.js

---
+++ Rewrite/Quantumlutx/Gchat.js
@@ -0,0 +1,25 @@
+/*
+
+[rewrite_local]
+
+^https?:\/\/api\.gchatapp\.net\/api\/(init|v1\/premium-users\/\w+-\w+-\w+-\w+-\w+\/check) url script-response-body https://raw.githubusercontent.com/QXPublic/MyUtilize/main/Rewrite/Quantumlutx/ChatAI.js
+
+[mitm]
+hostname = api.gchatapp.net
+*/
+var body = $response.body;
+var url = $request.url;
+//会员
+if (url.indexOf('init') != -1) {
+    var obj = JSON.parse(body);
+    obj.default_package_name =  ;
+    body = JSON.stringify(obj);
+} else if (url.indexOf('v1/premium-users/\w+-\w+-\w+-\w+-\w+/check) url script') != -1) {
+    var obj = JSON.parse(body);
+    obj.is_trial = false;
+    obj.expired_date = "2099-01-15 13:13:31";
+    obj.auto_renew_status = true ;
+    body = JSON.stringify(obj);
+
+}
+$done({ body });
