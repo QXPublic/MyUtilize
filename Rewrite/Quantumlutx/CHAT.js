ab6d6e13cdddac2523800614b3fb9b3078ec0871
oooo <105234248+QXPublic@users.noreply.github.com>
2024-01-20 22:50:04 +0800

    Update CHAT.js

---
+++ Rewrite/Quantumlutx/CHAT.js
@@ -0,0 +1,16 @@
+
+/*
+[rewrite_local]
+^https?:\/\/api\.chatapp\.dev\/users\/\w+\/\?hash=\w+&uid=\w+&v=[1-9]\.[0-9] url script-response-body https://raw.githubusercontent.com/QXPublic/MyUtilize/main/Rewrite/Quantumlutx/CHAT.js 
+
+[mitm]
+hostname = api.chatapp.dev
+
+*/
+
+var obj = JSON.parse($response.body);
+obj.user.subscription_valid = true;
+obj.user.is_plus = true;
+obj.user.valid_until = "4072603667";
+
+$done({body : JSON.stringify(obj)});
